f199e2e9b334ff716ed79ad351761a5e045f0101
Kyle Chen <kylec641@gmail.com>
2022-02-19 00:24:29 -0800

    Added sphere indicators to RayShooter script

--- OurFirstProject/Assets/RayShooter.cs
+++ OurFirstProject/Assets/RayShooter.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class RayShooter : MonoBehaviour
 {
-		private Camera cam;
+    private Camera cam;
 
-		void Start()
-		{
-				cam = GetComponent<Camera>();
-		}
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
-		void Update()
-		{
-				if (Input.GetMouseButtonDown(0))
-				{
-						Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
-						Ray ray = cam.ScreenPointToRay(point);
-						RaycastHit hit;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
+            Ray ray = cam.ScreenPointToRay(point);
+            RaycastHit hit;
 
-						if (Physics.Raycast(ray, out hit))
-						{
-								Debug.Log("Hit " + hit.point);
-						}
-				}
-		}
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
 }
