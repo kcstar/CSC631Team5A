4074fbd61d05656053017b2826a5fa43a5c4408f
Kyle Chen <kylec641@gmail.com>
2022-02-19 00:42:13 -0800

    Added cursor lock, and visual indicator for aiming

--- OurFirstProject/Assets/RayShooter.cs
+++ OurFirstProject/Assets/RayShooter.cs
@@ -9,6 +9,16 @@ public class RayShooter : MonoBehaviour
     void Start()
     {
         cam = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = cam.pixelWidth / 2 - size / 4;
+        float posY = cam.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
     }
 
     void Update()
