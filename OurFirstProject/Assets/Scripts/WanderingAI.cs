896f808de277258e3776c9ea5f1ea1fddd2e40ff
Kyle Chen <kylec641@gmail.com>
2022-02-21 22:24:01 -0800

    Added enemy alive state

--- OurFirstProject/Assets/Scripts/WanderingAI.cs
+++ OurFirstProject/Assets/Scripts/WanderingAI.cs
@@ -6,20 +6,34 @@ public class WanderingAI : MonoBehaviour
 {
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
+    private bool isAlive;
+
+    void Start()
+    {
+        isAlive = true;
+    }
 
     void Update()
     {
-        transform.Translate(0, 0, speed * Time.deltaTime);
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-
-        if (Physics.SphereCast(ray, 0.75f, out hit))
+        if (isAlive)
         {
-            if (hit.distance < obstacleRange)
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                float angle = Random.Range(-110, 110);
-                transform.Rotate(0, angle, 0);
+                if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
             }
         }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        isAlive = alive;
     }
 }
